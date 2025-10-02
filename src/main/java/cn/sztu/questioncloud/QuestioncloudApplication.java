f4264211a0db7fa1a2e7d6ce1f76aa1a5625b72e
eeeevmb <eeeevmb1123@gmail.com>
2025-10-02 13:47:48 +0800

    WIP...

---
+++ src/main/java/cn/sztu/questioncloud/QuestioncloudApplication.java
@@ -0,0 +1,13 @@
+package cn.sztu.questioncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestioncloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestioncloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/sztu/questioncloud/QuestioncloudV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sztu.questioncloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestioncloudV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
