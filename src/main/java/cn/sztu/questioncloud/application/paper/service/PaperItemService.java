937928a85d7bd12d7fae5d4a9e601c926085353c
Saleriy <SaleriyYuluo@outlook.com>
2026-01-02 22:31:43 +0800

    feat:增加随机组卷预览接口(无 Save/Delete 操作)

--- src/main/java/cn/sztu/questioncloud/application/paper/service/PaperItemService.java
+++ src/main/java/cn/sztu/questioncloud/application/paper/service/PaperItemService.java
@@ -1,6 +1,7 @@
 package cn.sztu.questioncloud.application.paper.service;
 
 import cn.sztu.questioncloud.web.rest.v1.paper.req.PaperItemSaveReq;
+import cn.sztu.questioncloud.web.rest.v1.paper.req.RandomBuildReq;
 import cn.sztu.questioncloud.web.rest.v1.paper.vo.PaperItemSaveVO;
 import cn.sztu.questioncloud.web.rest.v1.paper.vo.PaperItemVO;
 
@@ -12,8 +13,12 @@ import java.util.List;
  * @author Saler1y
  */
 public interface PaperItemService {
+
+    // === 自由组卷 ===
+
     /**
      * 存储试卷题目
+     * 若要新增、更改部分题目，请调用此方法
      *
      * @param paperId 试卷ID
      * @param reqs 存储试卷题目请求
@@ -30,10 +35,31 @@ public interface PaperItemService {
     void deleteItemsByPaperId(Long paperId);
 
     /**
-     * 根据试卷ID获取试卷题目详情 列表
+     * 根据试卷ID获取试卷题目详情列表
      *
      * @param paperId 试卷ID
      * @return 试卷题目列表
      */
     List<PaperItemVO> getItemsByPaperId(Long paperId);
+
+    // === 随机组卷 ===
+    /**
+     * 纯随机组卷
+     * 仅返回题目列表，不存储试卷题目，供前端预览使用
+     *
+     * @param paperId 试卷ID
+     * @param req 随机组卷请求
+     * @return 试卷题目列表
+     */
+    List<PaperItemSaveVO> previewRandomItems(Long paperId, RandomBuildReq req);
+
+    // === 暂时废弃 ===
+    /*
+      更改指定试题分数 (暂时废弃)
+
+      param paperId 试卷ID
+      param req 试题分数更改请求
+
+      void updateItemScore(Long paperId, PaperItemSaveReq req);
+     */
 }
