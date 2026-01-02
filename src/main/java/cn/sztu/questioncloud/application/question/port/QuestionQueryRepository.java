c8e7edafac11cc7ef717d64b3002759f587ac548
Saleriy <SaleriyYuluo@outlook.com>
2026-01-02 22:31:43 +0800

    feat:增加随机组卷预览接口(无 Save/Delete 操作)

--- src/main/java/cn/sztu/questioncloud/application/question/port/QuestionQueryRepository.java
+++ src/main/java/cn/sztu/questioncloud/application/question/port/QuestionQueryRepository.java
@@ -1,11 +1,13 @@
 package cn.sztu.questioncloud.application.question.port;
 
+import cn.sztu.questioncloud.application.question.enums.QuestionTypeEnum;
 import cn.sztu.questioncloud.common.model.vo.PageResult;
 import cn.sztu.questioncloud.web.rest.v1.question.req.QuestionInCollectionPageQuery;
 import cn.sztu.questioncloud.web.rest.v1.question.vo.QuestionDetailVO;
 import cn.sztu.questioncloud.web.rest.v1.question.vo.QuestionSummaryVO;
 import cn.xbatis.core.mybatis.mapper.context.Pager;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -45,4 +47,13 @@ public interface QuestionQueryRepository {
     */
     Pager<QuestionSummaryVO> findPageByCollectionId(Long collectionId,
                                                     QuestionInCollectionPageQuery query);
+
+    /**
+     *
+     * 根据题集IDs和题目类型查询题目ID列表
+     * @param collectionIds 题集IDs
+     * @param typeCode 题目类型(调用方法前先确保typeCode合法)
+     * @return 题目ID列表
+     */
+    List<QuestionSummaryVO> findIdsByCollectionsAndType(List<Long> collectionIds, String typeCode);
 }
