bd7c4e362bb7401546ead23016c8ddf9c278d1db
eeeevmb <eeeevmb1123@gmail.com>
2026-02-17 05:26:28 +0800

    feat: 修改用户注册接口，返回注册响应对象并新增RegisterVO类

---
+++ src/main/java/cn/sztu/questioncloud/web/rest/v1/user/vo/RegisterVO.java
@@ -0,0 +1,10 @@
+package cn.sztu.questioncloud.web.rest.v1.user.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegisterVO {
+    private Long userId;
+}
